0f00e491de15da7df811ea4322539e0921b54263
amgg <16143061+adrianmgg@users.noreply.github.com>
2023-08-25 13:43:00 -0700

    nvim - whichkey config changes, add gp/gP

--- neovim/lua/amgg/plugins/which-key.lua
+++ neovim/lua/amgg/plugins/which-key.lua
@@ -5,15 +5,25 @@ local function telescope_builtin(picker)
 	end
 end
 
-local whichkey_keymap = {
-	['<leader>'] = {
-		f = {
-			name = 'find',
-			t = { '<cmd>Telescope<cr>', 'telescope commands' },
-			f = { telescope_builtin'find_files', 'local files' },
-			b = { telescope_builtin'buffers', 'buffers' },
-			c = { telescope_builtin'colorscheme', 'color schemes' },
-			g = { telescope_builtin'live_grep', 'grep' },
+local whichkey_keymaps = {
+	{
+		opts = { prefix = '<leader>' },
+		mappings = {
+			f = {
+				name = 'find',
+				t = { '<cmd>Telescope<cr>', 'telescope commands' },
+				f = { telescope_builtin'find_files', 'local files' },
+				b = { telescope_builtin'buffers', 'buffers' },
+				c = { telescope_builtin'colorscheme', 'color schemes' },
+				g = { telescope_builtin'live_grep', 'grep' },
+			},
+		},
+	},
+	{
+		opts = { prefix = 'g' },
+		mappings = {
+			p = { '`[v`]', 'switch to VISUAL using last pasted text' },
+			P = { '`[V`]', 'switch to VISUAL LINE using last pasted text' },
 		},
 	},
 }
@@ -25,7 +35,9 @@ return {
 		config = function()
 			local which_key = require'which-key'
 			which_key.setup({})
-			which_key.register(whichkey_keymap)
+			for _, register_args in ipairs(whichkey_keymaps) do
+				which_key.register(register_args.mappings, register_args.opts)
+			end
 		end,
 	}
 }
